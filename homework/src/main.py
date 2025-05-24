ba399295eb80bbc74450d130611f2fb2b137d1f3
primolechuga <juanestebaca123@gmail.com>
2025-05-24 10:31:28 -0500

    fix main

--- homework/src/main.py
+++ homework/src/main.py
@@ -40,9 +40,11 @@ def main():
     ##
     working_directory = os.path.abspath(os.getcwd())
     mlflow_runs_path = os.path.join(working_directory, "my_mlruns")
+
     if not os.path.exists(mlflow_runs_path):
         os.makedirs(mlflow_runs_path)
-    mlflow.set_tracking_uri(mlflow_runs_path)
+
+    mlflow.set_tracking_uri("file:" + mlflow_runs_path)
 
     ## Autotracking para sklearn
     mlflow.sklearn.autolog(
@@ -99,10 +101,3 @@ def main():
         mlflow.log_metric("test_r2", r2)
 
         ## Ya no se requiere
-        # save_model_if_better(model, x_test, y_test)
-
-
-if __name__ == "__main__":
-    main()
-    
-    
